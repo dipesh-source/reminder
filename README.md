94cf55e9e510266f1af7ca910d6d9ef0e7e6d2f6
dipesh-source <parmardipesh2002@gmail.com>
2023-03-28 22:24:57 +0530

    Initialize a first commit with DB

---
+++ README.md
@@ -0,0 +1,32 @@
+#access token
+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
+
+Daily task
+
+id: an auto-incrementing primary key.
+task: the text of the reminder.
+reminder_datetime: the date and time the reminder should occur.
+
+
+weekly task
+
+id: an auto-incrementing primary key.
+task: the text of the reminder.
+day_of_week: the day of the week the reminder should occur, with 0 representing Monday.
+reminder_datetime: the date and time the reminder should occur.
+
+Monthly task
+
+id: an auto-incrementing primary key.
+task: the text of the reminder.
+start_date: the date of the month when the reminder period starts, in the format "YYYY-MM-DD".
+end_date: the date of the month when the reminder period ends, in the format "YYYY-MM-DD".
+reminder_datetime: the date and time the reminder should occur.
+
+Yearly task
+
+id: an auto-incrementing primary key.
+task: the text of the reminder.
+month: the month of the year when the reminder should occur (1-12).
+day_of_month: the day of the month when the reminder should occur (1-31).
+reminder_datetime: the date and time the reminder should occur.
---
+++ reminder_db.py
@@ -0,0 +1,60 @@
+""" Create a table for syatem. """
+import sqlite3
+
+# Connect to the database
+with sqlite3.connect("reminders.db") as conn:
+
+    # Create the daily reminders table
+    conn.execute(
+        """
+        CREATE TABLE IF NOT EXISTS daily_reminders (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        task TEXT NOT NULL,
+        reminder_datetime DATETIME NOT NULL
+        );
+        """
+    )
+
+    # Create the weekly reminders table
+    conn.execute(
+        """
+        CREATE TABLE IF NOT EXISTS weekly_reminders (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        task TEXT NOT NULL,
+        day_of_week INTEGER NOT NULL,
+        reminder_datetime DATETIME NOT NULL
+        );
+        """
+    )
+
+    # Create the monthly reminders table
+    conn.execute(
+        """
+        CREATE TABLE IF NOT EXISTS monthly_reminders (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        task TEXT NOT NULL,
+        start_date DATE NOT NULL,
+        end_date DATE NOT NULL,
+        reminder_datetime DATETIME NOT NULL
+        );
+        """
+    )
+
+    # Create the yearly reminders table
+    conn.execute(
+        """
+        CREATE TABLE IF NOT EXISTS yearly_reminders (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        task TEXT NOT NULL,
+        month INTEGER NOT NULL,
+        day_of_month INTEGER NOT NULL,
+        reminder_datetime DATETIME NOT NULL
+        );
+        """
+    )
+
+    # Commit the changes
+    conn.commit()
+
+# Close the connection
+conn.close()
